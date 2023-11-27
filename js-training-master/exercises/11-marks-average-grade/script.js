ab3c67c17b58a435f95e36d4906c6a3a4492a3d6
sobhiEgbaria <sobhim686@gmail.com>
2023-11-27 12:27:09 +0200

    ex to 11

--- js-training-master/exercises/11-marks-average-grade/script.js
+++ js-training-master/exercises/11-marks-average-grade/script.js
@@ -1,5 +1,3 @@
-
-
 const MARKSARRAY = [55, 85, 55, 65];
 
 /*
@@ -7,10 +5,15 @@ const MARKSARRAY = [55, 85, 55, 65];
   and returns the average of the marks in the array.
  */
 function calculateAverage(currentArray) {
-  // your code here
+  let sum = 0;
+  for (let i = 0; i < currentArray.length; i++) {
+    sum += currentArray[i];
+  }
+  avg = sum / currentArray.length;
+  return avg;
 }
 
-
+// console.log(calculateAverage(MARKSARRAY));
 /*
   the function calculateGrades() takes an array of marks as an argument
   and returns the grade of the student based on the average of the marks
@@ -22,7 +25,17 @@ function calculateAverage(currentArray) {
   - average <= 100 = 'A'
  */
 function calculateGrades(_currentArray) {
-  // your code here
+  let avg = calculateAverage(_currentArray);
+
+  if (avg < 70) {
+    return "D";
+  } else if (avg < 80) {
+    return "C";
+  } else if (avg < 90) {
+    return "B";
+  } else if (avg <= 100) {
+    return "A";
+  }
 }
 
-console.log('Grade:',calculateGrades(MARKSARRAY)); // should return 'C'
+console.log("Grade:", calculateGrades(MARKSARRAY)); // should return 'C'
