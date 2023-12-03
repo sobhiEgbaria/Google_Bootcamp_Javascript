2bf98a9ca14f5166de982c1a8420951b88860d99
sobhiEgbaria <sobhim686@gmail.com>
2023-12-03 14:47:33 +0200

    to do list with functions

--- day_7/day_7.js
+++ day_7/day_7.js
@@ -1,14 +1,59 @@
-// //Way to Clone an Array
-// const numbers = [1, 2, 3, 4, 5];
-//
-// // Old way
-// const clone_numbers = numbers.slice();
-//
-// // ES6 way
-// const clone_numbers_es6 = [...numbers];
-//
-//
+// to do list with function
 
+// class that make a one task
+const to_do_list = [];
 
-let a = [1,3,4,5,6]
-console.log(a)
+const add = (name, is_done = false) => {
+  to_do_list.push({
+    name: name,
+    is_done: is_done,
+    id: to_do_list.length + 1,
+    date: new Date().toLocaleDateString(),
+  });
+};
+
+const remove = (task_id) => {
+  to_do_list.forEach((task, index) => {
+    if (task.id == task_id) {
+      to_do_list.splice(index, 1);
+    }
+  });
+};
+
+const is_done = (task_id) => {
+  to_do_list.forEach((task, index) => {
+    if (task.id == task_id) {
+      task["is_done"] = true;
+    }
+  });
+};
+
+const editTodo = (task_id, update) => {
+  to_do_list.forEach((task) => {
+    if (task.id == task_id) {
+      task["name"] = update;
+    }
+  });
+};
+
+const search = (task_id) => {
+  for (const task of to_do_list) {
+    if (task.id == task_id) {
+      return task;
+    }
+  }
+};
+
+add("sobhi");
+add("egbaria");
+add("remove_test");
+add("update_test");
+
+console.log(to_do_list);
+remove(3);
+console.log(to_do_list);
+is_done(2);
+console.log(to_do_list);
+editTodo(4, "the update is don");
+console.log(to_do_list);
+console.log(search(1));
