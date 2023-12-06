38ae5dc3383ee6acbfa7e8bd5adf0e5251fc5e74
sobhiEgbaria <sobhim686@gmail.com>
2023-12-06 23:01:36 +0200

    to do app adding filter

--- day_8_to_do_list_ui/app.js
+++ day_8_to_do_list_ui/app.js
@@ -5,8 +5,9 @@ let to_do_list = [];
 if you remove the comment it will work just fine, but the layout not that good,
 and the completed button act act weird in some cases,
 that not happens when you enter the tasks manually
-that's why i leave it in comment: 
-you can try, sorry for the long speech :)
+that's why i leave it in comment.
+you can try uncomment the fetch.
+sorry for the long speech :)
 `;
 
 // // fetch the data and add it to the list then render the list to show thw data.
@@ -32,14 +33,14 @@ add_task.addEventListener("click", () => {
     add(input.value);
     input.value = "";
   }
-  render_task(to_do_list); // create the list from the beginning
+  render_task(to_do_list, to_do_list); // create the list from the beginning
 });
 
 // event listener to remove task
 tasks_div.addEventListener("click", (e) => {
   if (e.target.className == "delete") {
     remove(parseInt(e.target.id));
-    render_task(to_do_list); // create the list from the beginning
+    render_task(to_do_list, to_do_list); // create the list from the beginning
   }
 });
 
@@ -56,14 +57,16 @@ tasks_div.addEventListener("click", (e) => {
       e.target.className = "done";
     }
   }
+  render_task(to_do_list, to_do_list);
 });
 
 // // event listener to update the task ==> showing the update_bar
 tasks_div.addEventListener("click", (e) => {
   let id = parseInt(e.target.id); // stor the task id
   if (e.target.className == "update") {
-    update_content(id);
     update_bar.style.display = "block";
+    update_content(id);
+    update_input.value = "";
   }
 });
 
@@ -72,17 +75,75 @@ const update_content = (id) => {
   update_bar.addEventListener("click", (e) => {
     if (e.target.className == "heading_update_btn") {
       editTodo(id, update_input.value);
-      render_task(to_do_list); // create the list from the beginning
+      render_task(to_do_list, to_do_list); // create the list from the beginning
       update_bar.style.display = "none";
     }
   });
 };
 
+// // event listener to filter the task
+tasks_div.addEventListener("click", (e) => {
+  if (e.target.className == "Completed_btn") {
+    let filtered_list = to_do_list.filter((task) => {
+      return task.completed == true;
+    });
+
+    render_task(filtered_list, to_do_list);
+  }
+
+  if (e.target.className == "Active_btn") {
+    console.log(e.target.className);
+    let filtered_list = to_do_list.filter((task) => {
+      return task.completed == false;
+    });
+    render_task(filtered_list, to_do_list);
+  }
+
+  if (e.target.className == "all_btn") {
+    render_task(to_do_list, to_do_list);
+  }
+});
+
 //clear all the tasks first ( tasks_div.innerHTML = "";)
 // loop over the to_do_list and create the every new task/update_input/buttons
-const render_task = (to_do_list) => {
+// if the list not empty create a filter btns
+`the  `;
+const render_task = (render_list, to_do_list) => {
+  let is_Active = 0;
+  let is_Completed = 0;
+  for (const task of to_do_list) {
+    if (task.completed == true) {
+      is_Completed++;
+    } else {
+      is_Active++;
+    }
+  }
   tasks_div.innerHTML = "";
-  to_do_list.forEach((task) => {
+  if (to_do_list.length != 0) {
+    //create div_filter for the filters_btn
+    let div_filter = document.createElement("div");
+    div_filter.className = "div_filter";
+    tasks_div.appendChild(div_filter);
+
+    //create a filters btns
+    let all_btn = document.createElement("button");
+    all_btn.className = "all_btn";
+    all_btn.innerHTML = `ALL: ${to_do_list.length}`;
+    div_filter.appendChild(all_btn);
+
+    let Active_btn = document.createElement("button");
+    Active_btn.className = "Active_btn";
+    Active_btn.innerHTML = `Active: ${is_Active}`;
+    div_filter.appendChild(Active_btn);
+
+    let Completed_btn = document.createElement("button");
+    Completed_btn.className = "Completed_btn";
+    Completed_btn.innerHTML = `Completed: ${is_Completed}`;
+    div_filter.appendChild(Completed_btn);
+  }
+
+  //loop to create every new task
+  render_list.forEach((task) => {
     // create a div that contain one new task
     let new_task = document.createElement("div");
     new_task.className = "new_task";
@@ -90,7 +151,7 @@ const render_task = (to_do_list) => {
 
     // create the title of the new task
     let content = document.createElement("h4");
-    content.innerHTML = `${task.title}`;
+    content.innerHTML = `<i class="fa-sharp fa-solid fa-thumbtack"></i> ${task.title}`;
     new_task.appendChild(content);
 
     // div that contain all the 3 btns
@@ -186,8 +247,4 @@ const search = (task_id) => {
 
 const clear_all = () => {
   to_do_list.splice(0);
-};
-
-const get_items = () => {
-  return to_do_list;
 };
