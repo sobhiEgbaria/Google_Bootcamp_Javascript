0be330c1a669de0e4dcd35b8fbb057f5c4219e2f
sobhiEgbaria <sobhim686@gmail.com>
2025-04-27 10:06:42 +0300

    starting with nodejs section

--- day_15_secret_message_App/app.js
+++ day_15_secret_message_App/app.js
@@ -1,7 +1,10 @@
 const { hash } = window.location;
-const m = atob(hash.replace("#", ""));
-console.log(m);
-
+const message = atob(hash.replace("#", ""));
+if (message) {
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#m-show").classList.remove("hide");
+  document.querySelector("h1").innerHTML = message;
+}
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   document.querySelector("#message-form").classList.add("hide");
