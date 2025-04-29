1622b2ed41b2d9d710d9effa174f5458b3ed964d
sobhiEgbaria <sobhim686@gmail.com>
2025-04-29 14:07:32 +0300

    get and post methods

--- day_17_E_commerce_App/app.js
+++ day_17_E_commerce_App/app.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-  res.send("hi there");
+  res.send(`
+    <form method="POST">
+      <div>
+        <input name = "Email" placeholder="Email" />
+        <input name = "Password" placeholder="Password" />
+        <input name = "PasswordConfirmation" placeholder="Password Confirmation" />
+        <button>Sign In</button>
+      </div>
+    </form>
+    `);
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body);
 });
 let port = 3000;
 app.listen(port, () => {
