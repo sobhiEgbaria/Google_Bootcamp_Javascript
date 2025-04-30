1b8df0be826207bb9d57a1d4b5fa837fdb28abb3
sobhiEgbaria <sobhim686@gmail.com>
2025-04-30 15:57:58 +0300

    bild a getOne deleteOne methods

--- day_17_E_commerce_App/repositories/users.js
+++ day_17_E_commerce_App/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 class UsersRepository {
   constructor(fileName) {
     if (!fileName) {
@@ -19,11 +20,49 @@ class UsersRepository {
       })
     );
   }
+
+  async create(info) {
+    const records = await this.getAll();
+    info["id"] = this.randomId();
+    records.push(info);
+    await this.writeAll(records);
+  }
+
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.fileName,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((product) => product.id === id);
+  }
+
+  async deleteOne(id) {
+    const records = await this.getAll();
+    const newRecords = await records.filter((product) => product.id !== id);
+    await this.writeAll(newRecords);
+  }
+
+  async update(id, updateValue) {
+    const records = await this.getAll();
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  const data = await repo.getAll();
-  console.log(data);
+  await repo.create({ Email: "sobhi@", password: "12312312" });
+  await repo.create({ Email: "sobhi@", password: "12312312" });
+  const records = await repo.getAll();
+  const product = await repo.getOne("6d389545");
+  console.log(records);
+  console.log("===========================");
+  console.log(product);
+  // await repo.deleteOne("215cb7a7");
 };
 test();
