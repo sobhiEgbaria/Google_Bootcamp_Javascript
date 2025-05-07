d08d06b3f2dcab1d40878acb406d6d16b237cf30
sobhiEgbaria <sobhim686@gmail.com>
2025-05-07 19:06:35 +0300

    finsh calculator test

--- day_18_Js_unit_testing/test/calculator.test.js
+++ day_18_Js_unit_testing/test/calculator.test.js
@@ -1,1 +1,27 @@
-const { calculator } = require("./app.test");
+const { calculator } = require("../src/calculator");
+
+describe("test calculator", () => {
+  test("test the number params", () => {
+    expect(() => calculator("1", 1, "+")).toThrow();
+    expect(() => calculator(1, "1", "+")).toThrow();
+  });
+
+  test("check operators operation", () => {
+    const operators = ["+", "-", "*", "/"];
+
+    operators.forEach((operator) => {
+      const res = calculator(2, 3, operator);
+
+      expect(res).toBe(eval(`2 ${operator} 3`));
+    });
+  });
+
+  test("valid operator", () => {
+    const res = calculator(2, 3, "a");
+
+    expect(res).toMatchObject({
+      message: "invalid operator!",
+      success: false,
+    });
+  });
+});
